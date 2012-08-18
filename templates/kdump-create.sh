b1d83aaad00e96100376398059503bf8aa169a46
Jonas Bernoulli <jonas@bernoul.li>
2012-08-18 23:59:22 +0200

    add kdump template (wip)

---
+++ templates/kdump-create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Libraries required for dump.
+git clone git@github.com:tarsius/kdump.git
+git clone git://github.com/tarsius/keymap-utils.git
+git clone git://github.com/emacsmirror/load-dir.git
+git clone git://github.com/emacsmirror/naked.git
+git clone git://github.com/emacsmirror/org.git
+git clone git://github.com/tarsius/packed.git
